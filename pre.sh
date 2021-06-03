e7c5116f9bbaf1c6a985f0d70ccdafc6af774447
jefinneovibe <80023457+jefinneovibe@users.noreply.github.com>
2021-06-03 15:27:54 +0530

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo rm -r /var/www/html/*
-sudo rm -r /var/www/html/.*
+sudo rm -r /var/www/html/.editorconfig
+sudo rm -r /var/www/html/.env.example
+sudo rm -r /var/www/html/.gitattributes
+sudo rm -r /var/www/html/.gitignore
+sudo rm -r /var/www/html/.styleci.yml
+
