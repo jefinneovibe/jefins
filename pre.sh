f4c7154a1f0dd972b6f53a3eb429a8257ca2784a
jefinneovibe <jefin@neovibe.in>
2021-06-04 09:37:56 +0530

    sfas

--- pre.sh
+++ pre.sh
@@ -5,4 +5,5 @@ sudo rm -r /var/www/html/.env.example
 sudo rm -r /var/www/html/.gitattributes
 sudo rm -r /var/www/html/.gitignore
 sudo rm -r /var/www/html/.styleci.yml
+sudo rm -r /var/www/html/.env
 
